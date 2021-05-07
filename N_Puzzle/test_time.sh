0267394912df6c4df092664f21431e1d406300e4
MixFon <mixfon@rambler.ru>
2021-05-07 10:06:55 +0300

    add test_time.sh file

--- N_Puzzle/test_time.sh
+++ N_Puzzle/test_time.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-count=4
+count=10
 echo $count
 while [[ $count -gt 0 ]]
 do
 	echo $count
-	python2.7 npuzzle-gen.py -s 4 > temp
+	python2.7 npuzzle-gen.py -s 3 > temp
 	time ./puzzle temp >> /dev/null
 	let count=$count-1
 done
